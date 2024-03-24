d88738e20e4031cbb4337047a98afcb6b0d75226
ezheng <82283372+YellowBlackTea@users.noreply.github.com>
2024-03-24 04:01:17 +0100

    Fix function utils name

--- ability.py
+++ ability.py
@@ -1,6 +1,5 @@
 import re
-
-from data import save_data
+from utils import save_data
 
 class Attack:
     def __init__(self, attack_dict) -> None:
@@ -62,18 +61,34 @@ class Attack:
         self._cost = cost
         
     def __str__(self) -> str:
-        return f"{self.name}(Attack, {self.type}, Cost: {self.cost}): {self.description}"
+        return f"{self.name:<13} (Attack, {self.type}, Cost: {self.cost:<2}): {self.description}"
     
+    def attack_success(self):
+        pass
+        
 
 class Defense:    
     def __init__(self, defense_dict) -> None:
         self.name = defense_dict.get('Name')
         self.description = defense_dict.get('Description')
-        self.type = defense_dict.get('Type')
+        self.type = defense_dict.get('Element')
         self.healing_range = defense_dict.get('Healing')
-        self.accuracy = int(defense_dict.get('Accuracy'))
+        self.energy_range_range = defense_dict.get('Energy')
         self.cost = int(defense_dict.get('Cost'))
-        self.healing_min, self.healing_max = map(int, re.match(r"([0-9]+)?\s+-\s+([0-9]+)?", self.healing_range.groups()))
+        
+        # Extract min-max of the healing range, and set the energy_range to 0
+        try:
+            self.healing_min, self.healing_max = map(int, re.search(r"([0-9]+)?(?:\s+)?-(?:\s+)?([0-9]+)?", self.healing_range).groups())
+            self.energy_range_range = 0
+        except (AttributeError, TypeError):
+            self.healing_min, self.healing_max = None, None
+        
+        # Extract min-max of the energy_range range, and set the healing to 0 
+        try:
+            self.energy_range_min, self.energy_range_max = map(int, re.search(r"([0-9]+)?(?:\s+)?-(?:\s+)?([0-9]+)?", self.energy_range_range).groups())
+            self.healing_range = 0
+        except (AttributeError, TypeError):
+            self.energy_range_min, self.energy_range_max = None, None
     
     # Read access only
     @property
@@ -111,11 +126,11 @@ class Defense:
         
     # Read access only
     @property
-    def accuracy(self):
-        return self._accuracy
-    @accuracy.setter
-    def accuracy(self, accuracy):
-        self._accuracy = accuracy
+    def energy_range(self):
+        return self._energy_range
+    @energy_range.setter
+    def energy_range(self, energy_range):
+        self._energy_range = energy_range
         
     # Read access only
     @property
@@ -141,5 +156,21 @@ class Defense:
     def healing_max(self, healing_max):
         self._healing_max = healing_max
     
+    # Read access only
+    @property
+    def energy_min(self):
+        return self._energy_min
+    @energy_min.setter
+    def energy_min(self, energy_min):
+        self._energy_min = energy_min
+    
+    # Read access only
+    @property
+    def energy_max(self):
+        return self._energy_max
+    @energy_max.setter
+    def energy_max(self, energy_max):
+        self._energy_max = energy_max
+    
     def __str__(self) -> str:
-        return f"{self.name}(Defense, {self.type}, Cost: {self.cost}): {self.description}"
+        return f"{self.name:<13} (Defense, {self.type}, Cost: {self.cost}): {self.description}"
