da6c45cef4c38b85f0146c62ccaa18e073b31164
ezheng <82283372+YellowBlackTea@users.noreply.github.com>
2024-03-24 04:02:33 +0100

    Fix: utils function name

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pyfiglet
 
-from data import save_data
+from utils import save_data
 from player import Player
 
 
 def main():
     print(pyfiglet.figlet_format("Welcome to OOP-kemon", font = "slant", width = 250))
     pokemons = save_data("./data/pokemon.txt")
+    
     menu_screen(pokemons)
     #print(*pokemons[:3])
     #test = Pokemon(pokemons[1])
@@ -26,9 +27,9 @@ def menu_screen(pokemon_data: list[dict]):
     player.save_info()
     print("------------------------------------------------")
     # Print Menu option
-    print(f"{'1/ Random' : <10} - Change your Pokemon team")
-    print(f"{'2/ PvE' : <10} - Battle or Catch a wild Pokemon")
-    print(f"{'3/ PvP(AI)' : <10} - Battle another trainer")
+    print(f"{'1/ Random':<10} - Change your Pokemon team")
+    print(f"{'2/ PvE':<10} - Battle or Catch a wild Pokemon")
+    print(f"{'3/ PvP(AI)':<10} - Battle another trainer")
     #print("4/ Create another trainer")
     
 if __name__ == '__main__':
