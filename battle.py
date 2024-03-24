dc6a762148efc97545431588c4b249956cf4e348
ezheng <82283372+YellowBlackTea@users.noreply.github.com>
2024-03-24 04:02:08 +0100

    Fix some errors on methods

--- battle.py
+++ battle.py
@@ -1,14 +1,16 @@
 import random
 
-from data import save_data
-from utils import get_int
-from pokemon import Pokemon
+from utils import save_data, get_int
+
 from ability import Attack, Defense
+from player import Player
+from pokemon import Pokemon
 
 class Battle:
-    def __init__(self, self_pokemon: Pokemon, target_pokemon: Pokemon) -> None:
+    def __init__(self, self_pokemon: Pokemon, target_pokemon: Pokemon, player: Player) -> None:
         self.self_pokemon = self_pokemon
         self.target_pokemon = target_pokemon
+        self.player = player
     
     # Read access only
     @property
@@ -26,85 +28,114 @@ class Battle:
     def target_pokemon(self, target_pokemon):
         self._target_pokemon = target_pokemon
 
-    def calculate_coefficient(self) -> float:
+    # Read access only
+    @property
+    def player(self):
+        return self._player
+    @player.setter
+    def player(self, player):
+        self._player = player
+
+    def calculate_coefficient(self, ability: Attack) -> float:
         coefficients = {
             "Air": {"Air": 1, "Water": 0.5, "Fire": 1, "Earth": 1.5},
             "Water": {"Air": 1.5, "Water": 1, "Fire": 1, "Earth": 0.5},
             "Fire": {"Air": 0.5, "Water": 1.5, "Fire": 1, "Earth": 1},
             "Earth": {"Air": 1, "Water": 0.5, "Fire": 1.5, "Earth": 1}
         }
-        return float(coefficients[self.self_pokemon.type][self.self_pokemon.type])
+        return float(coefficients[self.self_pokemon.type][ability.type])
     
     def calculate_damage(self, ability: Attack) -> float:
         power = ability.power
-        coefficient = self.calculate_coefficient(self.target_pokemon.type)
+        coefficient = self.calculate_coefficient(ability)
 
-        damage = round(coefficient * random.uniform(0.85, 1) * (((power * 4 * (self.self_pokemon + 2)) / self.target_pokemon.resitance) + 2))
+        damage = round(coefficient * random.uniform(0.85, 1) * (((power * 4 * (self.target_pokemon.level + 2)) / self.target_pokemon.resistance) + 2))
         return damage
         
     def attack(self, ability: Attack) -> None:
         damage = 0
         if self.self_pokemon.current_energy >= ability.cost:
-            print(f"{self.self_pokemon.name} used {ability}!")
+            print(f"{self.self_pokemon.name} used {ability.name}!")
             self.self_pokemon.current_energy -= ability.cost
             
             if random.randint(0, 100) < ability.accuracy:
                 damage = self.calculate_damage(ability)
                 self.target_pokemon.current_hp -= damage
-                
+                print(f"{damage:>5} dealt!")
                 if self.target_pokemon.current_hp < 0:
                     self.target_pokemon.current_hp = 0
                     print(f"{self.target_pokemon.name} is KO!")
+                    return 1
             else:
                 print(f"{self.self_pokemon.name}'s attack missed!")
+                return None
         else:
             print(f"Not enough PP / Energy.")
-            
-        return self.self_pokemon, self.target_pokemon, damage
+            return None
 
     def defense(self, ability: Defense) -> None:
         restore_hp = 0
         restore_energy = 0
+
         if self.self_pokemon.current_energy >= ability.cost:
-            print(f"{self.self_pokemon.name} used {ability}!")
+            print(f"{self.self_pokemon.name} used {ability.name}!")
             
+            #print(ability.healing_min, ability.healing_max)
             restore_hp = random.randint(ability.healing_min, ability.healing_max)
-            self.self_pokemon.current += restore_hp
-            print(f"{self.self_pokemon.name} regained health!")
+            
+            # Only restore if health is not max
+            if self.self_pokemon.current_hp != self.self_pokemon.hp:
+                self.self_pokemon.current_hp += restore_hp
+                print(f"{self.self_pokemon.name} regained health!")
+            else:
+                print(f"{self.self_pokemon.name} health is already at max!")
+            
+            # Check Energy range does exist
             try:
-                restore_energy = random.randint(ability.healing_min, ability.healing_max)
-                self.current_energy += restore_energy
-                print(f"{self.self_pokemon.name} regained energy!")
+                restore_energy = random.randint(ability.energy_min, ability.energy_max)
+                if self.self_pokemon.current_energy != self.energy:
+                    self.current_energy += restore_energy
+                    print(f"{self.self_pokemon.name} regained energy!")
+                else:
+                    print(f"{self.self_pokemon.name} energy is already at max!")
             except Exception:
                 pass
         else:
             print(f"Not enough PP / Energy.")
         
-        return self.self_pokemon, restore_hp, restore_energy
-    
-    def change_pokemon(self, team) -> None:
-        changed_pkm = get_int("Which Pokemon to you want to change {self.self_pokemon} with? ")
-        while self.self_pokemon.name != team[changed_pkm].name:
-            print(f"{self.self_pokemon} is already in battle!")
-            changed_pkm = get_int("Which Pokemon to you want to change {self.self_pokemon} with? ")
+    def change_pokemon(self) -> None:
+        print(self.player)
+        changed_pkm = get_int(f"Which Pokemon do you want to change {self.self_pokemon.name} with? ")
+           
+        while self.self_pokemon.current_hp == 0:
+            print(f"{self.self_pokemon.name} is KO!")
+            changed_pkm = get_int(f"Which Pokemon to you want to change {self.self_pokemon.name} with? ")
+               
+        while self.self_pokemon.name == self.player.team[changed_pkm].name:
+            print(f"{self.self_pokemon.name} is already in battle!")
+            changed_pkm = get_int(f"Which Pokemon to you want to change {self.self_pokemon.name} with? ")
         
         temp_swap = self.self_pokemon
-        self.self_pokemon = team[changed_pkm]
-        team[changed_pkm] = temp_swap
+        self.self_pokemon = self.player.team[changed_pkm]
+        self.player.team[changed_pkm] = temp_swap
+        
+        print(f"{temp_swap.name} went back to his Pokeball.\n{self.self_pokemon.name} go!")
         
     def forfeit(self, player) -> int:
         print(f"You have forfeited. You lost!")
         return 0
     
-    def get_xp(self, combat='pvp'):
-        if combat == 'pvp':
-            levels = [self.target_pokemon.level]
-            
-            avg_levels = self.target_pokemon
+    def get_xp(self, xp_points: int) -> None:
+        self.self_pokemon.current_exp += xp_points
+        if self.self_pokemon.current_exp <= 100:
+            self.self_pokemon.level += 1
+            self.self_pokemon.hp += random.randint(1, 5)
+            self.self_pokemon.energy += random.randint(1, 5) 
+            self.self_pokemon.resistance += random.randint(1, 5)
     
     def start_battle_restore(self):
-        self.self_pokemon.current_hp = self.hp
-        self.target_pokemon.current_energy = self.energy
+        self.self_pokemon.current_hp = self.self_pokemon.hp
+        self.self_pokemon.current_energy = self.self_pokemon.energy
     
     def start(self):
         print("Battle starts!")
@@ -134,19 +165,7 @@ class Battle:
             print(f"{self.target_pokemon.name} wins!")
         else:
             print("It's a tie!")
-
-    def start(self):
-        print("Battle starts!")
-        self.self_pokemon.start
-    
-    def self_pokemon_turn(self):
-        print(f"{self.self_pokemon.name}'s turn:")
-        action = input("Choose an action (attack): ").lower()
-
-        if action == "attack":
-            move_index = random.randint(0, 2)
-            self.self_pokemon.attack(self.self_pokemon.skills[move_index], self.target_pokemon)
-        
+   
     def target_pokemon_turn(self):
         print(f"{self.target_pokemon.name}'s turn:")
         move_index = random.randint(0, 2)
@@ -157,8 +176,15 @@ class Battle:
     
 if __name__ == "__main__":
     pokemons = save_data("./data/pokemon.txt")
-    self_pokemon_pokemon = Pokemon(pokemons[0])  # Replace {} with appropriate dictionary for player 1's Pokemon
-    target_pokemon_pokemon = Pokemon(pokemons[1])  # Replace {} with appropriate dictionary for player 2's Pokemon
+    players = save_data("./data/trainer.txt")
+    #self_pokemon_pokemon = Pokemon(pokemons[0])  
+    target_pokemon_pokemon = Pokemon(pokemons[1])
 
-    battle = Battle(self_pokemon_pokemon, target_pokemon_pokemon)
-    battle.start()
+    player = Player('Joe')
+    player.randomise_team(pokemons)
+    self_pokemon_pokemon = player.team[0]
+    print(player)
+    print(self_pokemon_pokemon)
+    print("====")
+    battle = Battle(self_pokemon_pokemon, target_pokemon_pokemon, player)
+    battle.change_pokemon()
