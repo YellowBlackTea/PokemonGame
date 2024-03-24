17c22b4d1c0a9ee4d89b888b10a9eb13236076fd
ezheng <82283372+YellowBlackTea@users.noreply.github.com>
2024-03-24 04:03:35 +0100

    Add save_data function

--- utils.py
+++ utils.py
@@ -1,5 +1,23 @@
+import csv
+
+def save_data(filepath: str) -> list[dict]:
+    """Save data from its file path
+
+    Args:
+        filepath (str): path of the data file
+
+    Returns:
+        list[dict]: data stored as a dict and each row represents an index in the list
+    """
+    saved_data = []
+    with open(filepath, 'r') as file:
+        data = csv.DictReader(file, delimiter="\t")
+        for row in data:
+            saved_data.append(row)
+    return saved_data
+
 def get_int(prompt: str) -> int:
     while True:
         number = input(prompt)
-        if number.isdigit():
+        if number.isdecimal():
             return int(number)
